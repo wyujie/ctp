2ae857a17132c3bdcddc36da1ae7ef2b79258262
wyujie <yujiewu.cn@gmail.com>
2014-11-03 20:34:51 +0800

    Update make.sh

--- src/make.sh
+++ src/make.sh
@@ -23,7 +23,7 @@ cp ../ctp.swig ./
 swig -go -c++ -intgosize 64 -soname libctp.so ./ctp.swig
 $CC $CXXFLAGS -I../c++/ -I../helper/ -fpic -c ctp_wrap.cxx -o ctp_wrap.o
 $CC $CXXFLAGS -I../helper/ -fpic -c ../helper/helper.cpp -o helper.o
-$CC $LDFLAGS /usr/lib/thostmduserapi.so /usr/lib/thosttraderapi.so -shared -o libctp.so ctp_wrap.o helper.o
+$CC $LDFLAGS -shared -o libctp.so ctp_wrap.o helper.o /usr/lib/thostmduserapi.so /usr/lib/thosttraderapi.so
 cp ./libctp.so /usr/lib/
 
 
@@ -32,7 +32,7 @@ go tool 6c -I "$GODST/pkg/linux_amd64" -D _64BIT ctp_gc.c
 go tool 6g ctp.go
 go tool pack grc ctp.a ctp.6 ctp_gc.6
 cp ctp.a $GODST/pkg/linux_amd64/ctp.a
-mkdir $GODST/src/pkg/ctp
+mkdir -p $GODST/src/pkg/ctp
 cp ctp.go $GODST/src/pkg/ctp/
 
 
